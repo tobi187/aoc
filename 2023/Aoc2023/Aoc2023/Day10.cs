25f7364707fb38ae6ff1a0a091bf48413bb339a9
Tobias Fischer <fischerstobi1998@gmail.com>
2023-12-12 16:42:35 +0100

    fw

--- 2023/Aoc2023/Aoc2023/Day10.cs
+++ 2023/Aoc2023/Aoc2023/Day10.cs
@@ -9,7 +9,8 @@ namespace Aoc2023
 
         int x  = 0;
         int y = 0;
-        int steps = 0;
+        int steps = 1;
+        (int x, int y) last = (0, 0);
 
         internal void PartOne()
         {
@@ -18,23 +19,64 @@ namespace Aoc2023
 
             for (int y = 0; y < file.Length; y++)
                 for (int x = 0; x < file[0].Length; x++)
-                    if (file[y][x] == 's')
+                    if (file[y][x] == 'S')
                         (sx, sy) = (x, y);
 
-            
+            last = (sx, sy);
+            x = last.x + 1;
+            y = last.y;
+            while (!GotoNext()) 
+                steps++;
+
+            Console.WriteLine(Math.Ceiling(steps / 2.0));
         }
 
         internal bool GotoNext()
         {
+            var l = (x, y); 
             switch (file[y][x])
             {
-                case 'S': 
+                case 'S':
                     return true;
-                case '-'
-
-
+                case '-':
+                    if (last.x < x)
+                        x += 1;
+                    else
+                        x -= 1;
+                    break;
+                case '|':
+                    if (last.y < y)
+                        y += 1;
+                    else
+                        y -= 1;
+                    break;
+                case 'L':
+                    if (last.y < y)
+                        x += 1;
+                    else 
+                        y -= 1;
+                    break;
+                case 'J':
+                    if (last.y < y) 
+                        x -= 1;
+                    else
+                        y -= 1;
+                    break;
+                case 'F':
+                    if (last.y > y)
+                        x += 1;
+                    else
+                        y += 1;
+                    break;
+                case '7':
+                    if (last.y > y)
+                        x -= 1;
+                    else
+                        y += 1;
+                    break;
             }
-            
+            last = l;
+            return false;
         }
     }
 
--- 2023/Aoc2023/Aoc2023Fs/Day9.fsx
+++ 2023/Aoc2023/Aoc2023Fs/Day9.fsx
@@ -45,3 +45,30 @@ file
 |> List.map (fun l -> [l])
 |> List.map (findZeroesBackwards)
 |> List.sum
+
+
+// Wieland
+
+let difference (a,b) = b - a
+
+let extrapolate_row = List.pairwise >> List.map difference
+
+let extrapolate numbers =
+    if List.sum numbers = 0 then None else
+    Some (numbers, extrapolate_row numbers)
+
+let calc_next_value s ns = s + List.last ns
+
+let parse_line (s:string) =
+    s.Split " "
+    |> Array.toList
+    |> List.map int
+    |> List.unfold extrapolate
+    |> List.fold calc_next_value 0
+
+let start () =
+    File.ReadAllLines "input.txt"
+    |> Array.sumBy parse_line
+    |> printfn "%A (1702218515 is correct)"
+
+
