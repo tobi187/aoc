89213fcd765c09b7924f2e01a9e424351f433e9d
Tobias Fischer <fischerstobi1998@gmail.com>
2024-01-05 17:24:10 +0100

    :(

--- 2023/Aoc2023/Aoc2023/Day14.cs
+++ 2023/Aoc2023/Aoc2023/Day14.cs
@@ -11,7 +11,17 @@ namespace Aoc2023
         public char[][] file = File.ReadAllLines("in.txt").Select(x => x.ToArray()).ToArray();
         public void PartOne()
         {
-            ShiftNorth(1);
+            for (int i = 0; i < 1_000_000_000; i++)
+                Rotate();
+            Calc();
+        }
+
+        public void Rotate()
+        {
+            ShiftNorth(true);
+            ShiftEast(true);
+            ShiftNorth(false);
+            ShiftEast(false);
         }
 
         void Easy()
@@ -19,43 +29,82 @@ namespace Aoc2023
             for (int x = 0; x < file[0].Length; x++)
             {
                 int count = 0;
-                for (int y = ; y < file.Length; y++)
+                for (int y = 0; y < file.Length; y++)
                 {
 
                 }
             }
         }
 
-        void ShiftNorth(int dir)
+        void ShiftNorth(bool d)
         {
             var n = file.Select(x => x.Select(y => y == 'O' ? '.' : y).ToArray()).ToArray();
             
             for (int x = 0; x < file[0].Length; x++)
             {
                 int stoneCount = 0;
-                int lastStone = 0;
-                for (int y = 0; y < n.Length; y++)
+                int lastStone = d ? 0 : file.Length-1;
+                for (int y = d ? 0 : n.Length - 1; y < n.Length && y >= 0; y += d ? 1 : -1)
                 {
                     if (file[y][x] == 'O') stoneCount++;
                     else if (file[y][x] == '#')
                     {
-                        if (stoneCount == 0)
-                        {
-                            lastStone = y;
-                            continue;
-                        }
-                        //if (lastStone != 0) ++lastStone;
-                        for (int yy = lastStone; yy < lastStone + stoneCount; yy++)
-                            n[yy][x] = 'O';
+                        for (int yy = 0; yy < stoneCount; yy++)
+                            n[lastStone + yy * (d ? 1 : -1)][x] = 'O';
                         stoneCount = 0;
-                        lastStone = y;
+                        lastStone = y + (d ? 1 : -1);
                     }
                 }
-                for (int yy = lastStone; yy != lastStone + stoneCount; yy++)
-                    n[yy][x] = 'O';
+                for (int yy = 0; yy < stoneCount; yy++)
+                    n[lastStone + yy * (d ? 1 : -1)][x] = 'O';
             }
 
-            Console.WriteLine(string.Join("\n", n.Select(s => string.Concat(s))));
+            //Console.WriteLine(string.Join("\n", n.Select(s => string.Concat(s))));
+
+            file = n;
+        }
+
+        void ShiftEast(bool d)
+        {
+            var n = file.Select(x => x.Select(y => y == 'O' ? '.' : y).ToArray()).ToArray();
+
+            for (int y = 0; y < file[0].Length; y++)
+            {
+                int stoneCount = 0;
+                int lastStone = d ? 0 : file.Length - 1;
+                for (int x = d ? 0 : n.Length - 1; x < n.Length && x >= 0; x += d ? 1 : -1)
+                {
+                    if (file[y][x] == 'O') stoneCount++;
+                    else if (file[y][x] == '#')
+                    {
+                        for (int xx = 0; xx < stoneCount; xx++)
+                            n[y][lastStone + xx * (d ? 1 : -1)] = 'O';
+                        stoneCount = 0;
+                        lastStone = x + (d ? 1 : -1);
+                    }
+                }
+                for (int xx = 0; xx < stoneCount; xx++)
+                    n[y][lastStone + xx * (d ? 1 : -1)] = 'O';
+            }
+
+            //Console.WriteLine(string.Join("\n", n.Select(s => string.Concat(s))));
+
+            file = n;
+        }
+
+        void Calc(bool show = false)
+        {
+            if (show)
+                Console.WriteLine(string.Join("\n", file.Select(s => string.Concat(s))));
+
+            var g = file;
+            int s = 0;
+            for (int y = 0; y < g.Length; y++)
+            {
+                var c = g[y].Count(x => x == 'O');
+                s += c * (g.Length - y);
+            }
+            Console.WriteLine(s);
         }
     }
 }
--- 2023/Aoc2023/Aoc2023Fs/Day13.fsx
+++ 2023/Aoc2023/Aoc2023Fs/Day13.fsx
@@ -3,16 +3,26 @@ open System.IO
 
 let file = (File.ReadAllText "in.txt").Split "\r\n\r\n"
 
-let identical (a, b) = a = b
+let ident (a,b) = a = b
 
-let find l =
-    let dt = List.distinct l
-    0
+let rec findMaxAndCount (is, l, i1, i2) =
+    let ret () = (is - i1, is + 1)
+    if i1 < 0 || i2 >= List.length l then ret ()
+    elif l[i1] <> l[i2] then ret ()
+    else findMaxAndCount (is, l, (i1-1), (i2+1))
+    
 
-let calc ind rows =
-    let f, s = List.splitAt ind rows
-
-    f.Length
+let rec find l =
+    let pl = List.pairwise l
+    [
+        for i in 0..pl.Length-1 do
+            if ident pl[i] then yield i
+    ]
+    |> List.map (fun v -> (v, l, v, v+1))
+    |> List.map (findMaxAndCount)
+    |> List.sortByDescending (fst)
+    |> List.tryHead
+    |> Option.defaultValue (0, 0)
 
 let proces (grid:string) = 
     let rows =
@@ -25,10 +35,13 @@ let proces (grid:string) =
         |> List.transpose
         |> List.map (List.rev)
 
+    match find rows, find horRows with
+    | ((a, b), (c, d)) when a > c -> b * 100
+    | ((a, b), (c, d)) -> d
+
     
 file
-|> Array.map (proces)
-|> Array.sum
+|> Array.map (proces) 
     
     
-// 29857 -> too low
+// 31764 -> too low
